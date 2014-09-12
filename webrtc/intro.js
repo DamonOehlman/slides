46d048b762df0e44d77354292571a3cfdae72599
Damon Oehlman <damon.oehlman@gmail.com>
2014-09-12 10:13:19 +1000

    Refactor to be just code not markdown :)

--- webrtc/intro.js
+++ webrtc/intro.js
@@ -1,1 +1,15 @@
-module.exports = require('shaz').md(require('./intro.md'))
+var s = require('shaz');
+var fs = require('fs');
+
+module.exports = [
+  s().h2('What is').h1('WebRTC?'),
+  s().jpg(fs.readFileSync(__dirname + '/../images/network.jpg')),
+  s().h2('what can').h1('I build').h2('with WebRTC?'),
+  s().h1('Many Things').jpg(fs.readFileSync(__dirname + '/../images/bluelego.jpg')),
+  s().h2('From Video + Audio Apps'),
+  s().png(fs.readFileSync(__dirname + '/../images/hangouts.png')).contain(),
+  s().h2('To Super Cool Data Related Apps'),
+  s().png(fs.readFileSync(__dirname + '/../images/webtorrent.png')).contain(),
+  s().h1('Getting Started'),
+  s().h2('You could start with').a({ href: 'https://github.com/GoogleChrome/webrtc/blob/master/samples/web/js/adapter.js' }, 'adapter.js')
+];
