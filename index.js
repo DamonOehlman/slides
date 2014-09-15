e02b83f08ff1e6b9db0fbadbd93873e9cd9707a4
Damon Oehlman <damon.oehlman@gmail.com>
2014-09-15 09:07:33 +0300

    Style improvements

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var s = module.exports = require('shaz');
+var fs = require('fs');
 
 require('shaz-flickr')(s, { apikey: '1afeba19b3dfd2a8a3671deb8a6fa165' });
 require('shaz-npm')(s);
@@ -6,5 +7,7 @@ require('shaz-todo')(s);
 
 // use shaz-runnable for examples
 require('shaz-runnable')(s, {
+  cdn: 'http://localhost:3000',
+  iframeStyle: fs.readFileSync(__dirname + '/css/demo-iframe.css', 'utf8'),
   demoConsole: true
 });
